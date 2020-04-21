5b97a0b79f362d8e9d3e2b32214c7ac3145aaf68
Yuri <yuriypidlisnyi2020@gmail.com>
2020-04-21 22:05:39 +0300

    Added a greeting by name

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "<h1> Hello World!</h1>"
+
+@app.route("/<string:name>")
+def greeting(name):
+    return f"<h1> Hello {name}!</h1>"
 
 if __name__ == "__main__":
     app.run()
